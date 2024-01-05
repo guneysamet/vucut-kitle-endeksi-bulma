1967e8a8bd2e9cf5068629c034ec0b8d94004eeb
samet <samet@DESKTOP-1FM03Q2>
2024-01-05 14:31:22 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Boy : ");
+            double boy = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Kilo : ");
+            int kilo = Convert.ToInt32(Console.ReadLine());
+            double boy2 = boy / 100;
+            double boy2m = boy2 * boy2;
+            double vke = kilo / boy2m;
+            vke = Math.Round(vke, 1);
+            Console.WriteLine();
+            if (vke <= 18.5)
+            {
+                Console.WriteLine("Vücut kitle endeksiniz : {0}\nZayıfsınız.", vke);
+            }
+            else if (vke > 18.5 && vke <= 24.9)
+            {
+                Console.WriteLine("Vücut kitle endeksiniz : {0}\nNormal ağırlıktasınız.", vke);
+            }
+            else if (vke > 24.9 && vke <= 29.9)
+            {
+                Console.WriteLine("Vücut kitle endeksiniz : {0}\nKilolusunuz.", vke);
+            }
+            else
+            {
+                Console.WriteLine("Vücut kitle endeksiniz : {0}\nObezsiniz.", vke);
+            }
+            Console.Read();
+        }
+    }
+}
